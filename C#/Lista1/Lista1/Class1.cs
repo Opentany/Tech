b26ab615691757cd9d1b9a34ec646c2b889b9c33
Szary <opentany.1972@gmail.com>
2015-10-09 12:34:14 +0200

    I can write in C#

---
+++ C#/Lista1/Lista1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lista1
+{
+    public class Number
+    {
+        private readonly int _ourNumber;
+        private string _numberBased;
+
+        
+        public Number(int number)
+        {
+            _ourNumber = number;
+        }
+
+        public string ToBase(int Base)
+        {
+            if (Base >= 2 && Base <= 16)
+            {
+               _numberBased = Convert.ToString(_ourNumber, Base); 
+            }
+            else
+            {
+                throw new ArgumentException("Base must be between 2 and 16");
+            }
+            return _numberBased;
+        }
+
+        public bool SomethingMustBeFalse()
+        {
+            return false;
+        }
+
+    }
+}
---
+++ C#/Lista1/Lista1Project/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Lista1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lista1Project
+{
+    [TestClass]
+    public class NumberTests
+    {
+        readonly Number _num=new Number(144);
+        [TestMethod]
+        //MethodName_StateUnderTest_ExpectedBehavior
+        public void ToBase_ToBase2_True()
+        {
+            //arrange
+            var actual = _num.ToBase(2);
+            const string expected = "10010000";
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void ToBase_ToBase16_False()
+        {
+            //arrange
+            var actual = _num.ToBase(16);
+            const string notexpected = "91"; //true value of 114 in base16 is 90
+            //assert
+            Assert.AreNotEqual(notexpected,actual);
+        }
+
+        [TestMethod]
+        public void ToBase_ToBase19_ExceptionPlusFail()
+        {
+            try
+            {
+                var actual = _num.ToBase(19);
+            }
+            catch (ArgumentException ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void SomethingMustBeFalse_IsFalse_ReallyIs()
+        {
+            //arrange
+            var condition = _num.SomethingMustBeFalse();
+            //assert
+            Assert.IsFalse(condition);
+        }
+
+        [TestMethod]
+        public void _num_IsTypeOfNumber_ItIsNot()
+        {
+            Assert.IsNotInstanceOfType(_num.ToBase(2), typeof (Number));
+        }
+        
+    }
+}
