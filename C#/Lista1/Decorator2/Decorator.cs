e94a4c44243272d60d7f6f3297e9aacdea61a813
Szary <opentany.1972@gmail.com>
2015-11-13 00:03:08 +0100

    jeszcze dwa

---
+++ C#/Lista1/Decorator2/Decorator.cs
@@ -0,0 +1,18 @@
+﻿namespace Decorator2
+{
+    public abstract class Decorator : LibraryItem // The 'Decorator' abstract class
+    {
+        protected LibraryItem libraryItem;
+
+        // Constructor
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+    }
+}
---
+++ C#/Lista1/Lista1Project/Decorator2Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Decorator2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesty
+{
+    [TestClass]
+    public class Decorator2Test
+    {
+        Book book = new Book("Worley", "Inside ASP.NET", 10);
+
+ 
+      // Create video
+      Video video = new Video("Spielberg", "Jaws", 23, 92);
+
+        [TestMethod]
+        public void DecoTest()
+        {
+            book.Display();
+            video.Display();
+            // Make video borrowable, then borrow and display
+      Console.WriteLine("\nMaking video borrowable:");
+ 
+      Borrowable borrowvideo = new Borrowable(video);
+      borrowvideo.BorrowItem("Customer #1");
+      borrowvideo.BorrowItem("Customer #2");
+ 
+      borrowvideo.Display();
+        }
+      
+    }
+}
